1f7234281506a818300cab1e0f270a2ee24be3c4
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2025-09-19 12:26:10 +0200

    User service implementation (#5)
    * adding unit tests for service and controller and added code coverage to teh project
    
    * introducing code coverage on PR
    
    * introducing code coverage on PR
    
    * Update ci.yml
    
    * resolving build issues and addign jacoco plugin
    
    * disabling jacoco for order-service untill we write testcases for the service
    
    * Resolving build issues - adding mockBean for repository in test context
    
    * Resolving build issues - adding mockBean for repository in test context
    
    * Resolving build issues - adding mockBean for repository in test context

--- user-service/src/test/java/com/getyourfood/userservice/UserServiceApplicationTest.java
+++ user-service/src/test/java/com/getyourfood/userservice/UserServiceApplicationTest.java
@@ -1,10 +1,20 @@
 package com.getyourfood.userservice;
 
+import com.getyourfood.userservice.repository.UserRepository;
+import com.getyourfood.userservice.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
+@Import(UserService.class)
 public class UserServiceApplicationTest {
+
+  @MockBean private UserRepository userRepository;
+
+  @MockBean private PasswordEncoder passwordEncoder;
 
   @Test
   void contextLoads() {}
