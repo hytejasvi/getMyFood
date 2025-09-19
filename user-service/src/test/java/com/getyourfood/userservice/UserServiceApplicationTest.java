138cbfe8913ce66791810e53d96afe7a5ce9de4e
hytejasvi <hytejasvi@gmail.com>
2025-09-19 12:03:14 +0200

    Resolving build issues - adding mockBean for repository in test context

--- user-service/src/test/java/com/getyourfood/userservice/UserServiceApplicationTest.java
+++ user-service/src/test/java/com/getyourfood/userservice/UserServiceApplicationTest.java
@@ -1,10 +1,14 @@
 package com.getyourfood.userservice;
 
+import com.getyourfood.userservice.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 public class UserServiceApplicationTest {
+
+  @MockBean private UserRepository userRepository;
 
   @Test
   void contextLoads() {}
