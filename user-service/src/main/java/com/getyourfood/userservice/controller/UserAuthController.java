4d1c44e9c2377fd13bd06d6f95f1271757abb78a
hytejasvi <34671247+hytejasvi@users.noreply.github.com>
2025-09-20 14:14:36 +0200

    User service (#11)
    * implementing jwt util and other necessary additional data to teh entity
    
    * Update ci.yml
    
    * removing unwanted logger
    
    * adding addtional testcases

--- user-service/src/main/java/com/getyourfood/userservice/controller/UserAuthController.java
+++ user-service/src/main/java/com/getyourfood/userservice/controller/UserAuthController.java
@@ -26,7 +26,7 @@ public class UserAuthController {
   @PostMapping("/signup")
   public ResponseEntity<?> createUser(@Valid @RequestBody UserSignupDto userSignupDto) {
     userService.signUp(userSignupDto);
-    return ResponseEntity.status(HttpStatus.OK).build();
+    return ResponseEntity.status(HttpStatus.CREATED).build();
   }
 
   @PostMapping("/login")
--- user-service/src/test/java/com/getyourfood/userservice/service/UserServiceTest.java
+++ user-service/src/test/java/com/getyourfood/userservice/service/UserServiceTest.java
@@ -1,16 +1,18 @@
 package com.getyourfood.userservice.service;
 
 import static com.getyourfood.userservice.fixtures.UserTestBuilder.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
+import com.getyourfood.userservice.controller.dto.UserLoginDto;
 import com.getyourfood.userservice.controller.dto.UserSignupDto;
 import com.getyourfood.userservice.entity.User;
 import com.getyourfood.userservice.fixtures.UserTestBuilder;
 import com.getyourfood.userservice.repository.UserRepository;
 import com.getyourfood.userservice.service.exception.UserAlreadyRegisteredException;
+import com.getyourfood.userservice.service.exception.UserLoginException;
+import com.getyourfood.userservice.util.JwtUtil;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -26,6 +28,8 @@ public class UserServiceTest {
   @Mock private UserRepository userRepository;
 
   @Mock private PasswordEncoder passwordEncoder;
+
+  @Mock private JwtUtil jwtUtil;
 
   @InjectMocks private UserService userService;
 
@@ -49,6 +53,26 @@ public class UserServiceTest {
                         && user.getPhoneNumber().equals(userSignupDto.getPhoneNumber())
                         && user.getUserName().equals(userSignupDto.getName())
                         && user.getPassword().equals(DEFAULT_ENCODED_PASSWORD)));
+  }
+
+  @Test
+  void shouldReturnToken_ForValidCredentials() {
+
+    UserLoginDto loginDto = new UserTestBuilder().buildLoginDto();
+    User existingUser = new UserTestBuilder().buildUserEntity();
+
+    when(userRepository.findByEmailOrPhoneNumber(DEFAULT_PHONE_NUMBER, DEFAULT_PHONE_NUMBER))
+        .thenReturn(Optional.of(existingUser));
+    when(passwordEncoder.matches(DEFAULT_PASSWORD, DEFAULT_ENCODED_PASSWORD)).thenReturn(true);
+    when(jwtUtil.generateToken(anyLong(), anyString(), anyString())).thenReturn("fake.jwt.token");
+
+    String result = userService.userLogin(loginDto);
+
+    assertNotNull(result);
+    assertEquals("fake.jwt.token", result);
+    verify(jwtUtil)
+        .generateToken(
+            existingUser.getId(), existingUser.getEmail(), existingUser.getRole().toString());
   }
 
   @Test
@@ -78,5 +102,33 @@ public class UserServiceTest {
         assertThrows(UserAlreadyRegisteredException.class, () -> userService.signUp(userSignupDto));
 
     assertEquals("Phone Number already registered", ex.getMessage());
+  }
+
+  @Test
+  void shouldThrowLoginException_ForInvalidUsername() {
+    UserLoginDto loginDto = new UserLoginDto("wrong@email.com", DEFAULT_PASSWORD);
+
+    when(userRepository.findByEmailOrPhoneNumber("wrong@email.com", "wrong@email.com"))
+        .thenReturn(Optional.empty());
+
+    UserLoginException ex =
+        assertThrows(UserLoginException.class, () -> userService.userLogin(loginDto));
+
+    assertEquals("Invalid Login details", ex.getMessage());
+  }
+
+  @Test
+  void shouldThrowLoginException_ForInvalidPassword() {
+    UserLoginDto loginDto = new UserLoginDto(DEFAULT_EMAIL, "wrongPassword");
+    User existingUser = new UserTestBuilder().buildUserEntity();
+
+    when(userRepository.findByEmailOrPhoneNumber(DEFAULT_EMAIL, DEFAULT_EMAIL))
+        .thenReturn(Optional.of(existingUser));
+    when(passwordEncoder.matches("wrongPassword", DEFAULT_ENCODED_PASSWORD)).thenReturn(false);
+
+    UserLoginException ex =
+        assertThrows(UserLoginException.class, () -> userService.userLogin(loginDto));
+
+    assertEquals("Incorrect Password", ex.getMessage());
   }
 }
